c9f082ac264b4f5e75eed9bcf20309a676292772
Dang Linh <105706265+DangLjnh@users.noreply.github.com>
2024-10-13 14:25:34 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,74 +2,42 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const bodyParser = require('body-parser')
+
+// Enable CORS for all routes
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  next()
+})
 
 server.use(middlewares)
-server.use(bodyParser.json())
 
-// Add custom rewrite routes
+// Log all requests
+server.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+  '/api/*': '/$1',
+  '/blog/:resource/:id/show': '/:resource/:id'
 }))
 
-// Custom POST route for creating a new resource
-server.post('/api/:resource', (req, res) => {
-    const resource = req.params.resource;
-    const db = router.db; // Lowdb instance
-    const data = req.body;
-
-    const collection = db.get(resource);
-    if (!collection) {
-        return res.status(404).send('Resource not found');
-    }
-
-    const created = collection.insert(data).write();
-    res.status(201).json(created);
-});
-
-// Custom PUT route for updating a resource by ID
-server.put('/api/:resource/:id', (req, res) => {
-    const resource = req.params.resource;
-    const id = req.params.id;
-    const db = router.db;
-    const data = req.body;
-
-    const collection = db.get(resource);
-    if (!collection) {
-        return res.status(404).send('Resource not found');
-    }
-
-    const updated = collection.find({ id }).assign(data).write();
-    if (!updated) {
-        return res.status(404).send('Resource not found');
-    }
-
-    res.status(200).json(updated);
-});
-
-// Custom DELETE route for deleting a resource by ID
-server.delete('/api/:resource/:id', (req, res) => {
-    const resource = req.params.resource;
-    const id = req.params.id;
-    const db = router.db;
-
-    const collection = db.get(resource);
-    if (!collection) {
-        return res.status(404).send('Resource not found');
-    }
-
-    const deleted = collection.remove({ id }).write();
-    if (!deleted.length) {
-        return res.status(404).send('Resource not found');
-    }
-
-    res.status(200).json({ message: 'Resource deleted successfully' });
-});
-
+// Use the router
 server.use(router)
+
+// Start the server
 server.listen(3000, () => {
-    console.log('JSON Server is running')
+  console.log('JSON Server is running on http://localhost:3000')
+  console.log('Available routes:')
+  console.log('GET    /api/{resource}')
+  console.log('GET    /api/{resource}/{id}')
+  console.log('POST   /api/{resource}')
+  console.log('PUT    /api/{resource}/{id}')
+  console.log('PATCH  /api/{resource}/{id}')
+  console.log('DELETE /api/{resource}/{id}')
 })
 
 // Export the Server API
